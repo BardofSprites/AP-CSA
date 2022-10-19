3f475b56596a4745dcec470e3d75b4b62a944520
BardofSprites <earlgreytea42@outlook.com>
2022-10-19 10:20:19 -0400

    Fixed a typo

--- Exercises/Super/Main.java
+++ Exercises/Super/Main.java
@@ -4,7 +4,7 @@ class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("Enter your the student name:");
-    String studentName = input.nextLine();
+    String name = input.nextLine();
     System.out.println("Enter your math score:");
     int math = input.nextInt();
     System.out.println("Enter your ela score:");
@@ -12,7 +12,7 @@ class Main {
     System.out.println("Enter your service hours:");
     int hours = input.nextInt();
 
-    HSStudent student = new HSStudent("Daniel", math, ela, hours);
+    HSStudent student = new HSStudent(name, math, ela, hours);
     System.out.println("Pass math? " + student.passMath());
     System.out.println("Pass ela? " + student.passEla());
     System.out.println("Complete service? " + student.completeService());
